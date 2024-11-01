e645059ce03355b3262d88ace461016f8df8f4d6
Mustafa GOK <gkmstf29@gmail.com>
2024-11-02 00:03:57 +0300

    KDV hesaplayan program yapıldı

--- KDVHesaplayanProgram/src/Main.java
+++ KDVHesaplayanProgram/src/Main.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class Main {
+
+
+    public static void main(String[] args) {
+        double tutar, kdvOran = 0, kdvliFiyat;
+        Scanner inp= new Scanner(System.in);
+
+        System.out.print("Tutarı giriniz: ");
+        tutar = inp.nextDouble();
+        if (tutar < 1000) {
+            kdvOran=0.18;
+        } else if (tutar>=1000) {
+            kdvOran=0.08;
+        }
+        kdvliFiyat = tutar + tutar*kdvOran;
+        System.out.println("KDV'li fiyat: " + kdvliFiyat);
+    }
 }
